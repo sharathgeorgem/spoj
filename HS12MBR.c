4eea91d1941fc1c486ffe56a69c08e1f14a9df32
Sharath George M <sharathgeorgem@gmail.com>
2016-08-02 22:19:09 +0530

    Half done!

--- HS12MBR.c
+++ HS12MBR.c
@@ -1,12 +1,34 @@
 #include<stdio.h>
 int main() {
-	int test, n;
+	int i, test, n, xpoint, ypoint, xcircle, ycircle, radius, x1, y1, x2, y2, flag=0;
+	int  x1rect=0, y1rect=0, x2rect=0, y2rect=0;
 	char figure;
 	scanf("%d",&test);
 	while(test--) {
-		scanf("%d",&n)
+		scanf("%d",&n);
 		while(n--) {
-
+			scanf("%c",&figure);
+			if(figure == 'p') {
+				scanf("%d%d", &xpoint, &ypoint);
+				if(flag == 0) {
+					x1rect = xpoint;
+					y1rect = ypoint;
+					x2rect = xpoint;
+					y2rect = ypoint;
+					flag = 1;
+				}
+				if(xpoint < x1rect)
+					x1rect = xpoint;
+				if(ypoint < y1rect)
+					y1rect = ypoint;
+				if(xpoint > x2rect)
+					x2rect = xpoint;
+				if(ypoint > y2rect)
+					y2rect = ypoint;
+			}
 		}
+		printf("%d %d %d %d\n", x1rect, y1rect, x2rect, y2rect);
+		flag = 0;
 	}
+	return 0;
 }
