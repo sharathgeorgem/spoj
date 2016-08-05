24617ea55c45b236b4913206f9ea32611bbc43fc
Sharath George M <sharathgeorgem@gmail.com>
2016-08-05 21:45:47 +0530

    Prime nums

--- prime.c
+++ prime.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 int main() {
-	int test, m, n, i;
-	scanf("%d",&test);
-	while(test--) {
-		scanf("%d%d", &m, &n);
-		
-		printf("\n");
+	int i, m, j, n;
+	scanf("%d%d", &m, &n);
+	int arr[n];
+	arr[0] = 0;
+	arr[1] = 0;
+	for(i=2;i<n;i++)
+		arr[i] = 1;
+	for(i=2; i*i<n; i++) {
+		if(arr[i]) {
+			for(j=i*i; j<n; j += i) {
+				arr[j] = 0;
+			}
+		}
 	}
+	for(i=2;i<n;i++) {
+		if(arr[i])
+			printf("%d\t",i);
+	}
+	printf("\n");
 return 0;
 }
